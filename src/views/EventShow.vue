543d8a3b62867b3d4636419874bf2a93e07abb09
Malte Wirz <mail@maltewirz.com>
2019-10-14 12:09:12 +0200

    example for in-component route guards

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -26,17 +26,23 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
+import NProgress from 'nprogress'
+
+import store from '@/store/store'
 
 export default {
   props: ['id'],
-  created() {
-    this.fetchEvent(this.id)
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start()
+    store.dispatch('event/fetchEvent', routeTo.params.id).then(() => {
+      NProgress.done()
+      next()
+    })
   },
   computed: mapState({
     event: state => state.event.event
   }),
-  methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
